b98491fc0e8448b47d0fbf4bd833fe4eb6601d53
mcatan01 <mcatan01@dcs.bbk.ac.uk>
2018-12-26 02:23:22 +0000

    all functions working

--- test_three_musketeers.py
+++ test_three_musketeers.py
@@ -9,45 +9,45 @@ M = 'M'
 R = 'R'
 _ = '-'
 
-board1 =  [ [_, _, _, M, _],
-            [_, _, R, M, _],
-            [_, R, M, R, _],
-            [_, R, _, _, _],
-            [_, _, _, R, _] ]
+board1 =  [ ['-','-', '-', 'M', '-'],
+            ['-', '-', 'R', 'M', '-'],
+            ['-', 'R', 'M', 'R', '-'],
+            ['-', 'R', '-', '-', '-'],
+            ['-', '-', '-', 'R', '-'] ]
 
-board2 = [ [_, R, M, R, _],
-	   [_, _, _, R, _],
-	   [M, _, _, _, _],
-	   [_, R, R, _, _],
-	   [_, _, _, R, M] ]
+board2 = [ ['-', 'R', 'M', 'R', '-'],
+	   ['-', '-', '-', 'R', '-'],
+	   ['M', '-', '-', '-', '-'],
+	   ['-', 'R', 'R', '-', '-'],
+	   ['-', '-', '-', 'R', 'M'] ]
 
-board3 = [ [_, _, _, M, _],
-	   [R, R, _, M, _],
-	   [R, _, _, _, _],
-	   [_, _, _, _, _],
-	   [R, _, _, M, _] ]
+board3 = [ ['-', '-', '-', 'M', '-'],
+	   ['R', 'R', '-', 'M', '-'],
+	   ['R', '-', '-', '-', '-'],
+	   ['-', '-', '-', '-', '-'],
+	   ['R', '-', '-', 'M', '-'] ]
 
 def test_create_board():
     create_board()
-    assert at((0,0)) == R
-    assert at((0,4)) == M
+    assert at((0,0)) is 'R'
+    assert at((0,4)) is 'M'
     #eventually add at least two more test cases
 
 def test_set_board():
     set_board(board1)
-    assert at((0,0)) == _
-    assert at((1,2)) == R
-    assert at((1,3)) == M
+    assert at((0,0)) is '-'
+    assert at((1,2)) is 'R'
+    assert at((1,3)) is 'M'
     set_board(board2)
-    assert at((1,1)) == _
-    assert at((2,0)) == M
-    assert at((4,3)) == R
+    assert at((1,1)) is '-'
+    assert at((2,0)) is 'M'
+    assert at((4,3)) is 'R'
 
 def test_get_board():
     set_board(board1)
-    assert board1 == get_board()
+    assert board1 is get_board()
     set_board(board2)
-    assert board2 == get_board()
+    assert board2 is get_board()
 
 def test_string_to_location():
     with pytest.raises(ValueError):
@@ -62,7 +62,7 @@ def test_location_to_string():
     with pytest.raises(ValueError):
         location_to_string((10,0))
     with pytest.raises(ValueError):
-	location_to_string((2,6))
+        location_to_string((2,6))
     assert location_to_string((0,2)) == 'A3'
     assert location_to_string((1,1)) == 'B2'
     assert location_to_string((4,3)) == 'E4'
@@ -77,7 +77,7 @@ def test_all_locations():
     assert len(all_locations()) == 25
     assert all_locations()[1] == (0,1)
     assert all_locations()[12] == (2,2)
-    assert all_locations()[20] == (4,4)
+    assert all_locations()[24] == (4,4)
     
 
 def test_adjacent_location():
@@ -88,9 +88,9 @@ def test_adjacent_location():
 def test_is_legal_move_by_musketeer():
     set_board(board1)
     with pytest.raises(ValueError):
-        at((2,2)) == 'M'
+        is_legal_move_by_musketeer((3,1), 'up')
     with pytest.raises(ValueError):
-        at((1,3)) == 'M'
+        is_legal_move_by_musketeer((1,4), 'right')
     assert is_legal_move_by_musketeer((2,2), 'right') is True
     assert is_legal_move_by_musketeer((1,3), 'right') is not True
     
@@ -98,22 +98,27 @@ def test_is_legal_move_by_musketeer():
 def test_is_legal_move_by_enemy():
     set_board(board2)
     with pytest.raises(ValueError):
-        at((1,3)) == 'R'
+        is_legal_move_by_enemy((0,2), 'down')
     with pytest.raises(ValueError):
-	at((4,3)) == 'R'
+        is_legal_move_by_enemy((2,3), 'right')
     assert is_legal_move_by_enemy((1,3), 'down') is True
     assert is_legal_move_by_enemy((4,3), 'right') is not True
 
 def test_is_legal_move():
     set_board(board1)	
-    assert is_legal_move((4,3), 'down') is not True
+    assert is_legal_move((0,3), 'right') is not True
     assert is_legal_move((4,3), 'left') is True
+    with pytest.raises(ValueError):
+        is_legal_move((0,0), 'right')
     
 
 def test_can_move_piece_at():
     set_board(board1)
     assert can_move_piece_at((1,3)) is True
-    assert can_move_piece_at((0,3)) is not True
+    set_board(board3)
+    assert can_move_piece_at((1,3)) is not True
+    with pytest.raises(ValueError):
+        can_move_piece_at((2,1))
     
 
 def test_has_some_legal_move_somewhere():
@@ -121,7 +126,7 @@ def test_has_some_legal_move_somewhere():
     assert has_some_legal_move_somewhere('M') is True
     assert has_some_legal_move_somewhere('R') is True
     set_board(board3)
-    asserthas_some_legal_move_somewhere('M') is not True
+    assert has_some_legal_move_somewhere('M') is not True
 
 def test_possible_moves_from():
     set_board(board1)
@@ -144,16 +149,13 @@ def test_all_possible_moves_for():
     set_board(board2)
     assert all_possible_moves_for('M') == [((0, 1), 'right'), ((0, 1), 'left'), ((4, 4), 'left')]
     set_board(board1)
-    assert all_possible_moves_for('R') == [((1, 2), 'up'), ((1, 2), 'left'), ((2, 1), 'up'), 
-					  ((2, 1), 'left'), ((2, 3), 'right'), ((2,3), 'down'),
-					  ((3, 1), 'right'), ((3, 1), 'down', ((3, 1), 'left'),
-					  ((4, 3), 'up'), ((4, 3), 'right'), ((4, 3), 'left')]
+    assert all_possible_moves_for('R') == [((1, 2), 'up'), ((1, 2), 'left'), ((2, 1), 'up'),((2, 1), 'left'), ((2, 3), 'right'), ((2,3), 'down'),((3, 1), 'right'), ((3, 1), 'down'), ((3, 1), 'left'),((4, 3), 'up'), ((4, 3), 'right'), ((4, 3), 'left')]
     
     
 def test_make_move():
     set_board(board1)
     make_move((1, 2), 'up')
-    assert board[0][2]) == 'R'
+    assert board[0][2] == 'R'
     make_move((2, 2),'right')
     assert board[2][3] == 'M'
     
--- three_musketeers.py
+++ three_musketeers.py
@@ -14,12 +14,18 @@
 
 def create_board():
     global board
+    global valid_row 
+    global valid_col
+    global directions
     """Creates the initial Three Musketeers board and makes it globally
        available (That is, it doesn't have to be passed around as a
        parameter.) 'M' represents a Musketeer, 'R' represents one of
        Cardinal Richleau's men, and '-' denotes an empty space."""
     m = 'M'
     r = 'R'
+    valid_row = ["A", "B", "C", "D", "E"]
+    valid_col = range(1,6)
+    directions = ["right", "down", "left", "up"] 
     board = [ [r, r, r, r, m],
               [r, r, r, r, r],
               [r, r, m, r, r],
@@ -44,18 +50,23 @@ def string_to_location(s):
        """
     row = s[0].upper()
     col = int(s[1])
-    valid_row = ["A","B","C","D","E"]
-    valid_col = range(1,6)
     if row not in valid_row or col not in valid_col:
         raise ValueError("Input out of the correct range")
-    return [(a,b) for a = valid_row.index(s[0]) and b = valid_col.index(s[1])]
+    (a, b) = (valid_row.index(row), valid_col.index(col))
+    return (a,b)
 
 def location_to_string(location):
     """Returns the string representation of a location.
     Similarly to the previous function, this function should raise
-    ValueError exception if the input is outside of the correct range
+    ValueError exception if the input is outside of the correct range.
+    @param location: 2-tuple location
+    @return: string location
     """
-    pass # Replace with code
+    
+    if location[0] not in range(0,5) or location[1] not in range(0,5):
+        raise ValueError("Location out of range.")
+    else:
+        return valid_row[location[0]] + str(valid_col[location[1]])
 
 def at(location):
     """Returns the contents of the board at the given location.
@@ -63,87 +74,218 @@ def at(location):
     return board[location[0]][location[1]]
 
 def all_locations():
-    """Returns a list of all 25 locations on the board."""
-    pass # Replace with code
+    """Returns a list of all 25 locations on the board.
+    @return list
+    """
+    result = []
+    for x in range(0,5):
+        for y in range(0,5):
+            result.append((x,y))
+    return result
 
 def adjacent_location(location, direction):
     """Return the location next to the given one, in the given direction.
        Does not check if the location returned is legal on a 5x5 board.
-       You can assume that input will always be in correct range."""
+    @param location: 2-tuple of integers
+    @param direction: string
+    @return: 2-tuple of integers
+    """
     (row, column) = location
-    pass # Replace with code
+    direction = direction.lower()
+    if direction == "right":
+        return (row, column+1)
+    elif direction == "down":
+        return (row+1, column)
+    elif direction == "left":
+        return (row, column-1)
+    elif direction == "up":
+        return (row-1, column)
 
 def is_legal_move_by_musketeer(location, direction):
     """Tests if the Musketeer at the location can move in the direction.
     You can assume that input will always be in correct range. Raises
-    ValueError exception if at(location) is not 'M'"""
-    pass # Replace with code
+    ValueError exception if at(location) is not 'M'
+    @param location: 2-tuple of integers
+    @param direction: string
+    @return boolean
+    """
+    if at(location) != "M":
+        raise ValueError("Not 'M' at location")
+
+    move_location = adjacent_location(location, direction)
+    if at(move_location) == "R":
+        return True
+    else:
+        return False
 
 def is_legal_move_by_enemy(location, direction):
     """Tests if the enemy at the location can move in the direction.
     You can assume that input will always be in correct range. Raises
-    ValueError exception if at(location) is not 'R'"""
-    pass # Replace with code
+    ValueError exception if at(location) is not 'R'.
+    @param location: 2-tuple integers
+    @param direction: string
+    @return boolean
+    """
+    if at(location) != "R":
+        raise ValueError("Not 'R' at location")
+
+    move_location = adjacent_location(location, direction)
+    if at(move_location) is "-":
+        return True
+    else:
+        return False
 
 def is_legal_move(location, direction):
     """Tests whether it is legal to move the piece at the location
     in the given direction.
-    You can assume that input will always be in correct range."""
-    pass # Replace with code
+    @param location: 2-tuple integers
+    @param direction: string
+    @return boolean
+    """
+    move_location = adjacent_location(location, direction)
+    (row,column) = move_location
+    player = at(location)
+    if player == "M":
+        return is_legal_move_by_musketeer(location, direction)
+    elif player == "R":
+        return is_legal_move_by_enemy(location, direction)
+    else:
+        raise ValueError('No player at location') 
 
 def can_move_piece_at(location):
     """Tests whether the player at the location has at least one move available.
-    You can assume that input will always be in correct range.
-    You can assume that input will always be in correct range."""
-    pass # Replace with code
+    ValueError raised if no player at location
+    @param location: 2-tuple integers
+    @return boolean
+    """
+    result = False
+    if at(location) == "M":
+        for _ in directions:
+            try:
+                if is_legal_move_by_musketeer(location, _) is True:
+                    result = True
+            except:
+                continue
+    elif at(location) == "R":
+        for _ in directions:
+            try:
+                if is_legal_move_by_enemy(location, _) is True:
+                    result = True
+            except:
+                continue
+    else:
+        raise ValueError("No player at location")
+    return result
 
 
 def has_some_legal_move_somewhere(who):
     """Tests whether a legal move exists for player "who" (which must
     be either 'M' or 'R'). Does not provide any information on where
     the legal move is.
-    You can assume that input will always be in correct range."""
-    pass # Replace with code
+    @param who: string
+    @return boolean
+    """
+    result = False
+    if who is not "M" and who is not "R":
+        raise ValueError("input must be either 'M' or 'R'")
+    for _ in all_locations():
+        if at(_) is who:
+            if can_move_piece_at(_) is True:
+                result = True
+                break
+    return result
 
 def possible_moves_from(location):
     """Returns a list of directions ('left', etc.) in which it is legal
-       for the player at location to move. If there is no player at
-       location, returns the empty list, [].
-       You can assume that input will always be in correct range."""
-    pass # Replace with code
+    for the player at location to move. If there is no player at
+    location, returns the empty list, [].
+    @param location: 2-tuple integers
+    @return list
+    """
+    result = []
+    for _ in directions:
+        try:
+            at(adjacent_location(location, _))
+        except:
+            continue
+        if is_legal_move(location, _) is True :
+                result.append(_)
+    return result
 
 def is_legal_location(location):
     """Tests if the location is legal on a 5x5 board.
-    You can assume that input will be a pair of integer numbers."""
-    pass # Replace with code
+    You can assume that input will be a pair of integer numbers.
+    @param location: 2-tuple integers
+    @return boolean
+    """
+    (row, col) = location
+    result = False
+    if (row >= 0 and row <= 4) and (col >= 0 and col <= 4):
+        result = True
+    return result 
     
 def is_within_board(location, direction):
     """Tests if the move stays within the boundaries of the board.
-    You can assume that input will always be in correct range."""
-    pass # Replace with code
+    You can assume that input will always be in correct range.
+    @param location: 2-tuple integers
+    @param direction: string
+    @return boolean
+    """
+    move_location = adjacent_location(location, direction)
+    return is_legal_location(move_location)
     
 def all_possible_moves_for(player):
     """Returns every possible move for the player ('M' or 'R') as a list
-       (location, direction) tuples.
-       You can assume that input will always be in correct range."""
-    pass # Replace with code
+    (location, direction) tuples.
+    You can assume that input will always be in correct range.
+    @param player: string
+    @return list
+    """
+    result = []
+    for loc in all_locations():
+        for dir in directions:
+            if at(loc) is player and is_legal_move(loc, dir) is True:
+                result.append((loc, dir))
+    return result
 
 def make_move(location, direction):
     """Moves the piece in location in the indicated direction.
     Doesn't check if the move is legal. You can assume that input will always
-    be in correct range."""
-    pass # Replace with code
+    be in correct range.
+    @param location: 2-tuple integers
+    @param direction: string
+    """
+    new_location = adjacent_location(location, direction)
+    player = at(location)
+    board[new_location[0]][new_location[1]] = player
 
 def choose_computer_move(who):
     """The computer chooses a move for a Musketeer (who = 'M') or an
-       enemy (who = 'R') and returns it as the tuple (location, direction),
-       where a location is a (row, column) tuple as usual.
-       You can assume that input will always be in correct range."""
-    pass # Replace with code
+    enemy (who = 'R') and returns it as the tuple (location, direction),
+    where a location is a (row, column) tuple as usual.
+    You can assume that input will always be in correct range.
+    @param who: string
+    @return 2-tuple integers
+    """
+    import random
+    all_moves = all_possible_moves_for(who)
+    choice = random.randint(0, len(all_moves))
+    return all_moves[choice]
 
 def is_enemy_win():
-    """Returns True if all 3 Musketeers are in the same row or column."""
-    pass # Replace with code
+    """Returns True if all 3 Musketeers are in the same row or column.
+    @return boolean
+    """
+    result = False
+    location_M = []
+    for _ in all_locations:
+        if at(_) is "M":
+            location_M.append(_)
+    if (location_M[0][0] == location[1][0] == location[2][0] or
+        location_M[0][1] == location[1][1] == location[2][1]):
+	    result = True
+    return result
+    	
 
 #---------- Communicating with the user ----------
 #----you do not need to modify code below unless you find a bug
